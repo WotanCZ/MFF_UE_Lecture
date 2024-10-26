1442a33981cb9d37da899c237bde8125ef021d15
Vojtech Cimbura <cimburavojtech@gmail.com>
2024-10-26 21:41:26 +0200

    [mff-ue-2024][Tetris] Project after the first cpp seminar 24.10.2024.

---
+++ Tetris_2024/Source/Tetris/TetrisBlock.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TetrisBlock.generated.h"
+
+UCLASS()
+class TETRIS_API ATetrisBlock : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATetrisBlock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY()
+	UStaticMeshComponent* MeshComponent;
+	
+	UPROPERTY(EditAnywhere)
+	UMaterialInstance* BlockMaterialInstance;
+	
+	UPROPERTY()
+	UMaterialInstanceDynamic* DynamicBlockMaterialInstance;
+	
+	UPROPERTY()
+	FColor BlockColor;
+	
+	UPROPERTY(EditAnywhere)
+	FName ColorParameterName;
+};
